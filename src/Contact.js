db088958778123391c6ab03e1dd4287a41531e3f
Becca Glades <beccaglades@gmail.com>
2023-06-19 18:40:45 -0600

    Prepare for GitHub Pages deployment

--- src/Contact.js
+++ src/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
         <h2>Contact Me</h2>
         <p>If you'd like to get in touch, please feel free to reach out to me using the contact information below.</p>
         <div className="contact-info">
-          <p>Email: rebecca.c.glades@gmail.com</p>
+          <p>Email: rebecca.c.glades@gmail.com </p>
           <p>Phone: 806-928-8430</p>
         </div>
       </div>
